915debdfcd53a4c6c68cdae47ba44dee0943fa1c
Qvardium <qward2du@gmail.com>
2017-09-17 11:31:19 +0600

    Добавлен алгоритм для быстрой сортировки

--- src/TheMainClass.java
+++ src/TheMainClass.java
@@ -1,6 +1,8 @@
 
 import utility.*;
 
+import java.util.Arrays;
+
 public class TheMainClass {
 
     final static int N = 10;    //константа для вычесления N-го числа Фибоначчи
@@ -8,8 +10,16 @@ public class TheMainClass {
     public static void main(String[] args){
 
         TheFibonachy theFibonachy = new TheFibonachy();
-        print(theFibonachy.getResult(N));
+        //print(theFibonachy.getResult(N));
 
+
+        Integer[] forQS = new Integer[]{4,2,8,1,3,7,6,5};
+
+        print("\n"+Arrays.toString(forQS)+"\n");
+        TheQuckSort theQuckSort = new TheQuckSort();
+        theQuckSort.quickSor(forQS);
+
+        print("\n"+Arrays.toString(forQS));
     }
 
     public static void print(Object o){
--- src/utility/TheQuckSort.java
+++ src/utility/TheQuckSort.java
@@ -2,4 +2,56 @@ package utility;
 
 public class TheQuckSort {
 
+    Integer[] temp;
+    int iterLeft=0,iterRight=0;
+
+    public void quickSor(Integer[] o){
+        temp=o;
+        recursion(0,o.length-1);
+        System.out.println("\n");
+    }
+
+    private void recursion(int left, int right) {
+        if(right-left<=0) return;
+        else {
+            long current = temp[right];
+            //printIter(iterLeft,left,right);
+            int part = findPart(left,right,current);
+            printIter(++iterLeft,left,right);
+            recursion(left,part-1);
+            printIter(++iterRight,left,right);
+            recursion(part+1,right);
+        }
+    }
+
+    private void printIter(int iter,int l, int r){
+        for (int i = 0; i < iter; i++) {
+            System.out.print("-");
+        }
+        System.out.println(" recursion("+l+", "+r+")");
+    }
+
+    private int findPart(int left, int right, long current) {
+        int leftP=left-1;
+        int rightP=right;
+        for(;;){
+
+            while (temp[++leftP]<current);
+
+            while (rightP>0 && temp[--rightP]>current);
+
+            if(leftP>=rightP)
+                break;
+            else
+                change(leftP,rightP);
+        }
+        if(leftP!=right)change(leftP,right);
+        return leftP;
+    }
+
+    private void change(int leftP, int rightP) {
+        Integer tt=temp[leftP];
+        temp[leftP]=temp[rightP];
+        temp[rightP]=tt;
+    }
 }
